32fe0953a7907d955fda838e5e27d5977f02b2e5
AndrewCandy <35431147+AndrewCandy@users.noreply.github.com>
2022-07-19 15:36:25 -0400

    Work on data format pipeline

--- import_data.py
+++ import_data.py
@@ -2,7 +2,7 @@
 Author: Neel Dhulipala, Andrew DeCandia
 Project: Air Partners
 
-Script for importing necessary data for air quality analysis for static reporting.
+Script for importing necessary data for HEPA data research
 """
 
 import json
@@ -11,6 +11,7 @@ from numpy import nan
 import quantaq
 from datetime import datetime
 import data_analysis.quantaq_pipeline as qp
+import re
 
 with open('token.txt', 'r') as f:
     token = f.read()
@@ -72,7 +73,7 @@ class DataImporter(object):
         return sensor_dict
 
 
-    def _data_month(self, start_date, end_date, sensor_sn):
+    def get_data_in_timeframe(self, start_date, end_date, sensor_sn):
         """
         Gets data for a specific sensor.
         If data doesn't already exist in a pickle file, data is pulled from QuantAQ API.
@@ -103,53 +104,107 @@ class DataImporter(object):
 
 
     def _str_to_datetime(self, date):
-        try:
-            format = '%m/%d/%Y %H:%M:%S'
-            date = datetime.strptime(date, format)
-        except:
-            try:
-                format = '%m/%d/%Y %H:%M'
-                date = datetime.strptime(date, format)
-            except:
-                try:
-                    format = '%m/%d/%Y'
-                    date = datetime.strptime(date, format)
-                except:
-                    # no date provided
-                    raise Exception("Parameter date is empty")
-        return date
+        return pd.to_datetime(date).date()
 
 
-    def get_PM_data(self):
-        """
-        Collects data from all sensors for the month.
-
-        :returns: A list of all sensors available from QuantAQ API
-        :returns: A dictionary with sensor serial numbers as keys and pandas dataframes containing sensor data as values
-        """
-        # Get list of sensors, where this function returns two pandas.DataFrames of the devices
-        df_sensor_list, _ = self.get_sensor_list()
-        # Simplify output of last function into list of sensors
-        sn_list = list(df_sensor_list.sn)
-        sn_count = len(sn_list)
-        sn_dict = {}
-
-        sensor_count = 1
-        # For every sensor, download DataFrame with data of that sensor and insert it into dictionary
-        for sn in sn_list:
-            # Print out sensor downloading progress
-            print('\rSensor Progress: {0} / {1}\n'.format(sensor_count, sn_count), end='', flush=True)
-            # If sensor data already exists in pickle file, use that
-            df = self._data_month(sn)
-            # Add new dataframe to dictionary
-            sn_dict[sn] = df
-            sensor_count+=1
-        print('\nDone!')
-        return sn_list, sn_dict
-
+#    def get_PM_data(self):
+#        """
+#        Collects data from all sensors for the month.
+#
+#       :returns: A list of all sensors available from QuantAQ API
+#        :returns: A dictionary with sensor serial numbers as keys and pandas dataframes containing sensor data as values
+#        """
+#        # Get list of sensors, where this function returns two pandas.DataFrames of the devices
+#        df_sensor_list, _ = self.get_sensor_list()
+#        # Simplify output of last function into list of sensors
+#        sn_list = list(df_sensor_list.sn)
+#        sn_count = len(sn_list)
+#        sn_dict = {}
+#
+#        sensor_count = 1
+#        # For every sensor, download DataFrame with data of that sensor and insert it into dictionary
+#        for sn in sn_list:
+#            # Print out sensor downloading progress
+#            print('\rSensor Progress: {0} / {1}\n'.format(sensor_count, sn_count), end='', flush=True)
+#            # If sensor data already exists in pickle file, use that
+#            df = self.get_data_in_timeframe(sn)
+#            # Add new dataframe to dictionary
+#            sn_dict[sn] = df
+#            sensor_count+=1
+#        print('\nDone!')
+#        return sn_list, sn_dict
 
 if __name__ == '__main__':
-    di = DataImporter()
-    sn_list = di.get_sensor_list()
-    sn_dict = di.create_dict(sn_list)
-    print(sn_dict)
+  
+  # Convert csv to usable dataframes
+  deploy = pd.read_csv("hepa-pckls/MOF-Curated-Deployments.csv")
+  
+  locations = {}
+  d = {"sensor_sn" : [], "location" : [], "sensor_install" : [],
+       "hepa_install" : [], "sensor_removal" : []}
+  current_location = ""
+  
+  for row in deploy.itertuples():
+    
+    # Check for dates by looking for strings that start with a number.
+    if re.search(r'^(\d+)', str(row.Address)):
+      current_location = row.Address
+      d = {"sensor_sn" : [], "location" : [], "sensor_install" : [],
+           "hepa_install" : [], "sensor_removal" : []}
+      
+    # Look to see if this is the last sensor at this location
+    if str(row.Sensors) == 'nan':
+      df = pd.DataFrame(data=d)
+      
+      # Filter out sensors lacking deployment info
+      df = df.dropna(axis=0, how='all')
+      df = df[df.sensor_sn != "Unknown"]
+      df = df[df.sensor_install != "Unknown"]
+      df = df[df.hepa_install != "Unknown"]
+      df = df[df.sensor_removal != "Unknown"]
+      locations[current_location] = df
+    
+    # Add relevant data to dictionary
+    d["sensor_sn"].append(row.Sensors)
+    d["location"].append(row.Location)
+    d["sensor_install"].append(row._5)
+    d["hepa_install"].append(row._6)
+    d["sensor_removal"].append(row._8)
+  
+  # Remove locations with no sensor deployment from end of file
+  locations = {key:val for key, val in locations.items() if not val.empty}
+  
+  di = DataImporter()
+  final = {}
+  
+  for address in locations:
+    final[address] = {}
+    df = locations[address]
+    
+    # Pull outdoor sensor data
+    df_out = df[df.location == "Outdoors"]
+    # Should always be one row
+    for row in df_out.itertuples():
+      df_out_data = di.get_data_in_timeframe(row.sensor_install, row.sensor_removal, row.sensor_sn)
+      df_out_data = df_out_data.add_prefix("outdoor_")
+    
+    # Look at indoor sensor data
+    df_in = df[df.location == "Indoors"]
+    
+    for row in df_in.itertuples():
+      filter_date = row.hepa_install
+      df_in_data = di.get_data_in_timeframe(row.sensor_install, row.sensor_removal, row.sensor_sn)
+      df_in_data = df_in_data.add_prefix("indoor_")
+      # Combine outdoor and indoor data into pairs
+      result = pd.concat([df_out_data, df_in_data], ignore_index=True, axis=1)
+      
+    
+      
+        
+        
+        
+    
+  
+  
+  
+  
